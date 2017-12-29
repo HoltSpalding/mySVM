8eaec5d41e60c968d402ae054ccd6674431ae463
Holt Spalding <holtspalding@gmail.com>
2017-12-29 17:36:49 -0500

    next commit

--- 2DSVMunoptimized.py
+++ 2DSVMunoptimized.py
@@ -1,2 +1,29 @@
-ssss
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use("dark_background")
+
+
+class mySVC:
+	def __init__(self):
+		print("Hello")
+
+	def fit(self, data):
+		pass
+	def predict(self, data):
+		pass
+
+
+
+#######################TEST PRINT GRAPH
+newsvm = mySVC()
+data = np.array([[1,1],[2,2]])
+fig = plt.subplot()
+fig.set_xlim(0,3)
+fig.set_ylim(0,3)
+for i in data:
+	for j in data[i-1]:
+		fig.scatter(j[0],j[1])
+
+plt.show()
 
